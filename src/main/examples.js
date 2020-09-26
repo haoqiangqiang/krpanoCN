d8388e52c6b6fbb6bb5b95679a0f740e03cdf69a
haoqiangqiang <826173335@qq.com>
2020-09-27 01:15:10 +0800

    添加案例左菜单

--- src/main/examples.js
+++ src/main/examples.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Main,
+    LeftMenu,
+    LeftMenuTitle,
+    Content,
+    LeftMenuItem,
+} from './style'
 
 function Examples() {
     return (
-        <div>example</div>
+        <Main>
+            <LeftMenu>
+                <Link to='/examples/'>
+                <LeftMenuTitle>Examples</LeftMenuTitle>
+                </Link>
+                <Link to='/examples/'>
+                <LeftMenuItem>Show All</LeftMenuItem>
+                </Link>
+                <Link to='/examples/?depthmap'>
+                <LeftMenuItem>Depthmap</LeftMenuItem>
+                </Link>
+                <Link to='/examples/?js'>
+                <LeftMenuItem>Javascript</LeftMenuItem>
+                </Link>
+                <Link to='/examples/?multires'>
+                <LeftMenuItem>Multiresolution</LeftMenuItem>
+                </Link>
+                <Link to='/examples/?panovideo'>
+                <LeftMenuItem>Panoramic Video</LeftMenuItem>
+                </Link>
+                <Link to='/examples/?vtour'>
+                <LeftMenuItem>Virtual Tours</LeftMenuItem>
+                </Link>
+                <Link to='/examples/?webvr'>
+                <LeftMenuItem>WebVR</LeftMenuItem>
+                </Link>
+            </LeftMenu>
+            <Content>
+
+            </Content>
+        </Main>
     );
 }
 
---
+++ src/main/style.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+    display: flex;
+    justify-content: center;
+    position: relative;
+    top: 207px;
+    left: 0;
+    right: 0;
+`
+
+export const LeftMenu = styled.div`
+    width: 250px;
+    margin: 20px 0;
+    padding-right: 20px;
+    text-align: right;
+`
+export const LeftMenuTitle = styled.a`
+    width: 100%;
+    display: block;
+    margin: 10px 0;
+    padding: 10px 0;
+    font-size: 14px;
+    font-weight: 600;
+    color: #333;
+    text-decoration: none;
+    cursor: pointer;
+    outline: 0;
+`
+
+export const LeftMenuItem= styled.a`
+    display: block;
+    width: 100%;
+    padding-bottom: 8px;
+    font-size: 14px;
+    color: #57595a;
+`
+
+export const Content = styled.div`
+    width: 650px;
+    height: 100px;
+`
